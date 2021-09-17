e6feaff75af1bf6c2564c1251fade21c2c1c05df
unknown <djossoujosiashod@gmail.com>
2021-09-17 22:52:16 +0100

    comment the api call

--- pharmacy/viewsets.py
+++ pharmacy/viewsets.py
@@ -58,6 +58,7 @@ class PharmacyViewSet(viewsets.ModelViewSet):
 
 @api_view(['GET'])
 def find_nearest_pharmacies(request):
-    ip = get_client_ip(request)
-    r = requests.get('https://ipinfo.io/' + ip +"?token=5b0b28296033b7")
-    return Response(r.json())
+    # ip = get_client_ip(request)
+    # r = requests.get('https://ipinfo.io/' + ip +"?token=5b0b28296033b7")
+    # 6.3654,2.4183
+    return Response("6.3654,2.4183")
