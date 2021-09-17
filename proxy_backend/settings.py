d5e0855ccac26b7ebd57b5173893a666495e309b
unknown <djossoujosiashod@gmail.com>
2021-09-17 18:17:20 +0100

    heroku deploy

--- proxy_backend/settings.py
+++ proxy_backend/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL =  os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS =[
 	BASE_DIR/'static'
