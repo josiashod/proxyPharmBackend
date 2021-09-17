d3aee0c8ba60c10bfe991cb9e5cf5835f1df2eb0
unknown <djossoujosiashod@gmail.com>
2021-09-17 15:30:07 +0100

    deploy on eroku

--- proxy_backend/settings.py
+++ proxy_backend/settings.py
@@ -33,7 +33,7 @@ env = environ.Env(
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
