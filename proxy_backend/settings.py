00c98e54b12f5bdb34a3311004e814083c4670a1
unknown <djossoujosiashod@gmail.com>
2021-09-17 17:14:57 +0100

    heroku fi deploment bug

--- proxy_backend/settings.py
+++ proxy_backend/settings.py
@@ -171,6 +171,7 @@ STATICFILES_DIRS =[
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
